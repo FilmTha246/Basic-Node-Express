8f2f4323c40e46905c5b118af5b702711b1112c6
FilmTha246 <thanapornc.7426@gmail.com>
2024-07-05 00:44:22 +0700

    Add json

--- app.js
+++ app.js
@@ -13,6 +13,13 @@ import { fileURLToPath } from 'url';
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+// การส่งข้อมูลภายในเว็บแอปพลิเคชันขั้นสูง
+// const contacts = require("./data/contacts.json");
+import contacts from './data/contacts.json' assert { type: 'json' };
+
+// สร้างตัวแปร contactRouter
+const contactRouter = express.Router();
+
 app.use(morgan('combined'));
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
@@ -20,6 +27,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
+
+// การ Navigate ระหว่างเว็บ และ สร้าง Router
+contactRouter.route("/").get((req, res) => {
+    res.render("contacts",
+        contacts,
+    );
+})
+
+contactRouter.route("/1").get((req, res) => {
+    res.send("Hi! I'm John");
+})
+
+app.use("/contactus", contactRouter)
 
 // จัดการกับ request ที่เข้ามา
 app.get("/", (req,res) => {
